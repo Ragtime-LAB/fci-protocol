b9377f3c4e5bbb6b4554155e1ef94e2c7e895fb8
WindWeaver <moonbite233@gmail.com>
2026-07-27 23:02:04 +0800

    simplify diagnostics

--- include/fci_protocol/arm/diagnostics.hpp
+++ include/fci_protocol/arm/diagnostics.hpp
@@ -10,18 +10,11 @@
 namespace fci::arm {
 
 struct JointDiag {
-    std::uint8_t status;
     std::uint8_t healthy;
-    std::uint8_t health_state;
-    std::uint8_t recovery_phase;
-    std::uint16_t rx_age_ms;
 };
 
 struct GripperDiag {
     std::uint8_t healthy;
-    std::uint8_t health_state;
-    std::uint8_t recovery_phase;
-    std::uint16_t rx_age_ms;
 };
 
 struct ArmDiagnostics {
@@ -30,11 +23,8 @@ struct ArmDiagnostics {
     std::uint32_t mode_entry_ms;
     std::uint8_t bus_healthy;
     std::uint8_t bus_state;
-    std::uint8_t recovery_count;
     std::uint16_t tx_err_count;
     std::uint16_t rx_err_count;
-    std::uint8_t unhealthy_joint_count;
-    std::uint8_t pending_cmd_count;
     JointDiag joints[6];
     GripperDiag gripper;
 };
